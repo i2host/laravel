272efdd813ed7f3d2aef7d26ad862a1903707067
Sari B <admin@i2-host.com>
2017-07-29 21:34:25 +0200

    First Controller

--- database/migrations/2017_07_29_182316_create_countries_table.php
+++ database/migrations/2017_07_29_182316_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email','60')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name','60');
+            $table->string('image','120');
+            $table->string('image_code','60');
+            $table->tinyInteger('premium');
+            $table->integer('sort');
+            $table->tinyInteger('active');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('countries');
     }
 }
